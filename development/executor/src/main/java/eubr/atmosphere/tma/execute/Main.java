8d83e9ce88f754994e2a10b2b2a1c6f412193a23
JorgeLuiz <jre-jl@hotmail.com>
2020-06-13 20:49:24 -0300

    Updating execute code

--- development/executor/src/main/java/eubr/atmosphere/tma/execute/Main.java
+++ development/executor/src/main/java/eubr/atmosphere/tma/execute/Main.java
@@ -27,8 +27,7 @@ public class Main
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         runConsumer();
     }
 

38557d1851fb1708fa410f526fbde4e60e6b1a87
JorgeLuiz <jre-jl@hotmail.com>
2020-07-04 23:18:03 -0300

    Criando actuator java invocando código python

--- development/actuators/demo-actuator-python/demo-actuator.py
+++ development/actuators/demo-actuator-python/demo-actuator.py
@@ -49,4 +49,4 @@ if __name__ == '__main__':
      setup_logging()
      logger = logging.getLogger(__name__)
      logger.info('Initializing Demo Actuator')
-     demoActuator.run(debug='True', host='0.0.0.0', port=8080)
+     demoActuator.run(debug='True', host='0.0.0.0', port=8081)
